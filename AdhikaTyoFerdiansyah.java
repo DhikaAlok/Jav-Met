e355a9e536aa3eeba41c86d69471b4d1066830d0
DhikaAlok <adhikaferdiansyah44@gmail.com>
2024-05-27 22:43:02 +0700

    2213010366 {AdhikaTyoFerdiansyah}.java

---
+++ AdhikaTyoFerdiansyah.java
@@ -0,0 +1,15 @@
+public class AdhikaTyoFerdiansyah {
+
+    public static void main(String[] args) {
+        String fullname = "Adhika Tyo Ferdiansyah";
+        String nim = "2213010366";
+        String team = "Jav-Met";
+        String role = "Programer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
